e6a833ad0d2db10b4d2ceb02d10a4ade8cfff553
Lee <lijian@baai.ac.cn>
2021-04-07 12:25:50 +0800

    又解决了一个bug

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module weibo
 
 go 1.16
+
+require github.com/deckarep/golang-set v1.7.1 // indirect
--- tasks/SuperCheckIn.go
+++ tasks/SuperCheckIn.go
@@ -9,6 +9,8 @@ import (
 	"regexp"
 	"sync"
 	"weibo/client"
+
+	mapset "github.com/deckarep/golang-set"
 )
 
 // @title         SuperCheckIn
@@ -25,6 +27,7 @@ func SuperCheckIn(w *client.WeiboClient, wg *sync.WaitGroup) {
 	go GetSuperTopics(w, ch)
 	for {
 		if item, ok := <-ch; ok {
+			log.Println(item)
 			data, err := w.SuperCheckin(item[1])
 			//响应例子{'code': '100000', 'msg': '已签到', 'data': {'tipMessage': '今日签到，经验值+4', 'alert_title': '今日签到 第14482名', 'alert_subtitle': '经验值+4', 'alert_activity': ''}}
 			if err != nil {
@@ -50,21 +53,21 @@ func SuperCheckIn(w *client.WeiboClient, wg *sync.WaitGroup) {
 // @param         ch         chan [2]string            管道，返回[超话名称, 超话id]
 // @return
 func GetSuperTopics(w *client.WeiboClient, ch chan<- [2]string) {
+	defer close(ch)
 	reg := regexp.MustCompile("[0-9a-z]{38}")
 	sinceId := ""
+	allTopics := mapset.NewSet()
 	for {
 		data, err := w.ContainerGetIndex("100803_-_followsuper", sinceId)
 		if err != nil {
 			log.Println("获取超话列表异常:" + err.Error())
 			break
 		}
-		//python里直接["data"]["cardlistInfo"]["since_id"]
 		sinceId, ok := data["data"].(map[string]interface{})["cardlistInfo"].(map[string]interface{})["since_id"]
 		if !ok {
 			log.Println("获取超话列表sinceId错误")
 			break
 		}
-		//python里直接["data"]["cards"][0]["card_group"]
 		cards, ok := data["data"].(map[string]interface{})["cards"].([]interface{})
 		if !ok {
 			log.Println("获取超话列表cards错误")
@@ -83,7 +86,11 @@ func GetSuperTopics(w *client.WeiboClient, ch chan<- [2]string) {
 					if item["card_type"] == "8" {
 						name := item["title_sub"].(string)
 						id := reg.FindString(item["scheme"].(string))
+						if allTopics.Contains(name) {
+							return
+						}
 						ch <- [2]string{name, id}
+						allTopics.Add(name)
 					}
 				}
 			}
@@ -92,5 +99,4 @@ func GetSuperTopics(w *client.WeiboClient, ch chan<- [2]string) {
 			break
 		}
 	}
-	close(ch)
 }
